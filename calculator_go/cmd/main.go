166a44db68e218f1dd3ac08bfc167678f3673eb3
MKode312 <mark_kork@icloud.com>
2024-12-27 22:09:13 +0500

    init commit

---
+++ calculator_go/cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/MKode312/calculator_go/internal/application"
+)
+
+func main() {
+	app := application.New()
+	app.RunServer()
+}
---
+++ calculator_go/go.mod
@@ -0,0 +1,3 @@
+module github.com/Mkode312/calculator_go
+
+go 1.23.0
